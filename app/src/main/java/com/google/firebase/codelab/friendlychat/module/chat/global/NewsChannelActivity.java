99b5504a03a6f1a10b29dc4852b31fee2ad8611f
TheHuman20 <osad.sanya@gmail.com>
2018-07-06 00:00:03 +0300

    Update NewsChannelActivity.java

--- app/src/main/java/com/google/firebase/codelab/friendlychat/module/chat/global/NewsChannelActivity.java
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/module/chat/global/NewsChannelActivity.java
@@ -1,18 +1,3 @@
-/**
- * Copyright Google Inc. All Rights Reserved.
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.google.firebase.codelab.friendlychat.module.chat.global;
 
 import android.content.Intent;
@@ -310,14 +295,6 @@ public class NewsChannelActivity extends AppCompatActivity
                 return super.onOptionsItemSelected(item);
         }
     }
-
-//    @Override
-//    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-//        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
-//        // be available.
-//        Log.d(TAG, "onConnectionFailed:" + connectionResult);
-//        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
-//    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
